f34639231a84cbcdd3a1c33ed10fcdc2b4af6f41
Nevillested <olegsidnev@gmail.com>
2022-05-14 00:22:01 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_pi
+{
+    class lol {
+        void main() {
+            var txt = File.ReadAllText("C:/Users/Weabo/Desktop/digits.txt");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+}
